99d4bdbad9769dbe40a39e92939483fd2e935f36
Adeleke Israel <izzyfresher@gmail.com>
2022-01-25 23:45:06 +0100

    page post

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,15 @@ import Head from 'next/head'
 import Header from '../components/Header'
 import Star from '../assets/Images/star.png'
 import Image from 'next/image'
-import {sanityClient, urlFor} from '../sanity'
-import {Post} from '../typings'
+import { sanityClient, urlFor } from '../sanity'
+import { Post } from '../typings'
+import Link from 'next/link'
 
-interface Props { 
-  posts: Post[];
+interface Props {
+  posts: Post[]
 }
 
-
-export default function Home(props: Props) {
+export default function Home({ posts }: Props) {
   return (
     <div className="mx-auto max-w-7xl">
       <Head>
@@ -41,6 +41,27 @@ export default function Home(props: Props) {
           className="hidden h-32 p-10 md:inline-flex lg:h-full"
         />
       </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
+        {posts.map((post) => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div className="group cursor-pointer border rounded-lg overflow-hidden">
+              <img className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out" src={urlFor(post.mainImage).url()!} alt="" />
+
+              <div className="flex justify-between p-5 bg-white">
+                <div>
+                  <p className="text-lg font-bold">{post.title}</p>
+                  <p className="text-xs">
+                    {post.description} by {post.author.name}
+                  </p>
+                </div>
+
+                <img className="h-12 w-12 rounded-full" src={urlFor(post.author.image).url()!} alt="" />
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   )
 }
@@ -59,12 +80,11 @@ export const getServerSideProps = async () => {
   
   }`
 
-  const posts = await sanityClient.fetch(query);
+  const posts = await sanityClient.fetch(query)
 
-  return{
-    props:{
-      posts, 
-    }
+  return {
+    props: {
+      posts,
+    },
   }
-
 }
