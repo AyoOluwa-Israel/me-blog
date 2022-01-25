aabcc5cd82618ddca8120ede86e502dc15b6ebc0
Adeleke Israel <izzyfresher@gmail.com>
2022-01-25 23:58:39 +0100

    started  slug page

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -3,15 +3,29 @@ import Header from '../../components/Header'
 import { sanityClient, urlFor } from '../../sanity'
 import { Post } from '../../typings'
 
-interface Props { 
+interface Props {
   post: Post
 }
 
-function Post({post}: Props) {
-  console.log(post);
+function Post({ post }: Props) {
+  
   return (
     <main>
       <Header />
+
+      <img className="w-full h-40 object-cover" src={urlFor(post.mainImage).url()!} alt="" />
+
+      <article className="max-w-3xl mx-auto p-5">
+        <h1 className="text-3xl mt-10 mb-3">{post.title}</h1>
+        <h2 className="text-xl font-light text-gray-500 mb-2">{post.description}</h2>
+        <div className="flex items-center space-x-2">
+          <img className="h-10 w-10 rounded-full" src={urlFor(post.author.image).url()!} alt="" />
+          <p className="font-extralight text-sm">
+            Blog post by {" "}<span className="text-green-600">{post.author.name}</span> - 
+            Published at {new Date(post._createdAt).toLocaleString()}
+          </p>
+        </div>
+      </article>
     </main>
   )
 }
@@ -26,19 +40,19 @@ export const getStaticPaths = async () => {
     }
   }`
   const posts = await sanityClient.fetch(query)
-  const paths = posts.map((post: Post) =>({
+  const paths = posts.map((post: Post) => ({
     params: {
       slug: post.slug.current,
-    }
+    },
   }))
 
-  return { 
-    paths, 
-    fallback: "blocking",
+  return {
+    paths,
+    fallback: 'blocking',
   }
 }
 
-export const getStaticProps: GetStaticProps = async ({params}) => {
+export const getStaticProps: GetStaticProps = async ({ params }) => {
   const query = `*[_type == "post" && slug.current == $slug][0]{
     _id,
     _createdAt,
@@ -57,19 +71,20 @@ export const getStaticProps: GetStaticProps = async ({params}) => {
       body
   }
   `
-  const post = await sanityClient.fetch(query,{
+  const post = await sanityClient.fetch(query, {
     slug: params?.slug,
-  });
+  })
 
-  if (!post){
-    return{ 
+  if (!post) {
+    return {
       notFound: true,
     }
   }
 
-  return { 
+  return {
     props: {
-      post
-    }
+      post,
+    },
+    revalidate: 60, //after 60 seconds, it will reupdate the old cache version
   }
 }
