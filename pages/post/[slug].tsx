8c8d4b801645c4402045c1a078294f5e9ad506ce
Adeleke Israel <izzyfresher@gmail.com>
2022-01-26 00:26:22 +0100

    fixed form

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -2,30 +2,88 @@ import { GetStaticProps } from 'next'
 import Header from '../../components/Header'
 import { sanityClient, urlFor } from '../../sanity'
 import { Post } from '../../typings'
+import PortableText from 'react-portable-text'
 
 interface Props {
   post: Post
 }
 
 function Post({ post }: Props) {
-  
   return (
     <main>
       <Header />
 
-      <img className="w-full h-40 object-cover" src={urlFor(post.mainImage).url()!} alt="" />
+      <img
+        className="h-40 w-full object-cover"
+        src={urlFor(post.mainImage).url()!}
+        alt=""
+      />
 
-      <article className="max-w-3xl mx-auto p-5">
-        <h1 className="text-3xl mt-10 mb-3">{post.title}</h1>
-        <h2 className="text-xl font-light text-gray-500 mb-2">{post.description}</h2>
+      <article className="mx-auto max-w-3xl p-5">
+        <h1 className="mt-10 mb-3 text-3xl">{post.title}</h1>
+        <h2 className="mb-2 text-xl font-light text-gray-500">
+          {post.description}
+        </h2>
         <div className="flex items-center space-x-2">
-          <img className="h-10 w-10 rounded-full" src={urlFor(post.author.image).url()!} alt="" />
-          <p className="font-extralight text-sm">
-            Blog post by {" "}<span className="text-green-600">{post.author.name}</span> - 
+          <img
+            className="h-10 w-10 rounded-full"
+            src={urlFor(post.author.image).url()!}
+            alt=""
+          />
+          <p className="text-sm font-extralight">
+            Blog post by{' '}
+            <span className="text-green-600">{post.author.name}</span> -
             Published at {new Date(post._createdAt).toLocaleString()}
           </p>
         </div>
+
+        <div className="mt-10">
+          <PortableText
+            className=""
+            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
+            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
+            content={post.body}
+            serializers={{
+              h1: (props: any) => (
+                <h1 className="my-5 text-2xl font-bold" {...props} />
+              ),
+              h2: (props: any) => (
+                <h1 className="my-5 text-xl font-bold" {...props} />
+              ),
+              li: (children: any) => (
+                <li className="ml-4 list-disc ">{children}</li>
+              ),
+              link: ({ href, children }: any) => (
+                <a href={href} className="text-blue-500 hover:underline">
+                  {children}
+                </a>
+              ),
+            }}
+          />
+        </div>
       </article>
+
+      <hr className="my-5 mx-auto max-w-lg border border-yellow-500" />
+      <form className="flex flex-col p-5 max-w-2xl mx-auto mb-10">
+        <h3 className="text-sm text-yellow-500">Enjoyed This Article</h3>
+        <h4 className="text-3xl font-bold" >Leave a comment below</h4>
+        <hr className="py-3 mt-2"/>
+        <label className="block mb-5 ">
+          <span className="text-gray-700">Name</span>
+          <input className="shadow border rounded py-2 px-3 form-input mt-1  block w-full ring-yellow-500 outline-none focus:ring" placeholder="John Doe" type="text" />
+        </label>
+        <label className="block mb-5 ">
+          <span className="text-gray-700">Email</span>
+          <input className="shadow border rounded py-2 px-3 form-input mt-1  block w-full ring-yellow-500 outline-none focus:ring"  placeholder="John Doe" type="text" />
+        </label>
+
+        <label className="block mb-5 ">
+          <span className="text-gray-700">Comment</span>
+          <textarea className="shadow border rounded py-2 px-3 form-textarea mt-1 block w-full ring-yellow-500 outline-none focus:ring" placeholder="John Doe"  rows={8} />
+        </label>
+
+        
+      </form>
     </main>
   )
 }
